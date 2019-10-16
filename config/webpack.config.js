70415c245cc3f5904206066a0028c8b3bf340208
Benjamin Falk <benjamin.falk@cyanjoe.com>
2019-10-16 22:35:17 +0200

    feat: #4 process svelte files

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,17 +1,63 @@
 const path = require( 'path' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 
 const config = () => {
+  const env = process.env.NODE_ENV || 'development';
+  const isDev = env === 'development';
+  const isProd = env === 'production';
+
   return {
-    mode: process.env.NODE_ENV || 'development',
-    entry: './src/index.js',
+    mode: env,
+    devtool: isDev ? false : 'source-map',
+
+    entry: {
+      bundle: [ './src/index.js' ],
+    },
+
+    resolve: {
+      alias: {
+        svelte: path.resolve( __dirname, '../node_modules', 'svelte' ),
+      },
+      extensions: [ '.mjs', '.js', '.svelte' ],
+      mainFields: [ 'svelte', 'browser', 'module', 'main' ],
+    },
+
     output: {
-      filename: 'main.js',
+      filename: '[name].js',
+      chunkFilename: '[name].[id].js',
       path: path.resolve( __dirname, '../dist' ),
     },
+
+    module: {
+      rules: [
+        {
+          test: /\.svelte$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              emitCss: !isDev,
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+          ],
+        },
+      ],
+    },
+
     plugins: [
+      new MiniCssExtractPlugin( {
+        filename: '[name].css',
+      } ),
       new HtmlWebpackPlugin( {
         template: path.resolve( __dirname, '../public/index.html' ),
+        minify: true,
       } ),
     ],
   };
